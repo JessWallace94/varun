95dc3391b294f383e3879c461e5b9b21cd2c99d3
Jess Wallace <jw@jesswallace.co>
2020-01-18 13:05:29 +0000

    added new files and edited footer

---
+++ node_modules/@lekoarts/gatsby-theme-minimal-blog/src/components/footer.tsx
@@ -0,0 +1,40 @@
+/** @jsx jsx */
+import { jsx, Styled } from "theme-ui"
+import useSiteMetadata from "../hooks/use-site-metadata"
+
+const Footer = () => {
+  const { siteTitle } = useSiteMetadata()
+
+  return (
+    <footer
+      sx={{
+        boxSizing: `border-box`,
+        display: `flex`,
+        justifyContent: `space-between`,
+        mt: [6],
+        color: `secondary`,
+        a: {
+          variant: `links.secondary`,
+        },
+        flexDirection: [`column`, `column`, `row`],
+        variant: `dividers.top`,
+      }}
+    >
+      <div>
+        &copy; {new Date().getFullYear()} by {siteTitle}. all rights reserved.
+      </div>
+      <div>
+
+        {` `}
+        Customised with ♥ by
+        {` `}
+        <Styled.a aria-label="Link to the developers twitter" href="https://twitter.com/jesswallaceuk">
+          @jesswallaceuK
+        </Styled.a>
+
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ node_modules/@lekoarts/gatsby-theme-minimal-blog/src/components/homepage.tsx
@@ -0,0 +1,48 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import { Link } from "gatsby"
+import Layout from "./layout"
+import Hero from "../texts/hero"
+import Bottom from "../texts/bottom"
+import Contact from "../texts/contact"
+
+import Title from "./title"
+import Listing from "./listing"
+import List from "./list"
+import useSiteMetadata from "../hooks/use-site-metadata"
+import replaceSlashes from "../utils/replaceSlashes"
+
+type PostsProps = {
+  posts: {
+    slug: string
+    title: string
+    date: string
+    tags?: {
+      name: string
+      slug: string
+    }[]
+  }[]
+}
+
+const Homepage = ({ posts }: PostsProps) => {
+  const { basePath, blogPath } = useSiteMetadata()
+
+  return (
+    <Layout>
+      {/* <section sx={{ mb: [5, 6, 7], p: { fontSize: [1, 2, 3], mt: 2 } }}>
+        <Hero />
+      </section> */}
+      <Title text="my thoughts">
+        <Link to={replaceSlashes(`/${basePath}/${blogPath}`)}>read all posts</Link>
+      </Title>
+      <Listing posts={posts} showTags={false} />
+      {/* <List>
+        <Bottom />
+      </List> */}
+      <Contact />
+
+    </Layout>
+  )
+}
+
+export default Homepage
